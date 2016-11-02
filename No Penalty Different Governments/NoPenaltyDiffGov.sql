d65a146cf1083c9c1116dafd9d954a3b0c577b80
angelodgreat <carions46@gmail.com>
2016-11-02 18:49:46 +0800

    Added No Penalty and minor changes

---
+++ No Penalty Different Governments/NoPenaltyDiffGov.sql
@@ -0,0 +1,13 @@
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_AUTOCRACY';
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_OLIGARCHY';
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_CLASSICAL_REPUBLIC';
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_MONARCHY';
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_THEOCRACY';
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_MERCHANT_REPUBLIC';
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_FASCISM';
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_COMMUNISM';
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_DEMOCRACY';
+--JFD Governments you can remove this lines below if you're not playing with JFD's Rule of Faith
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_JFD_ABSOLUTE_MONARCHY';
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_JFD_CONSTITUTIONAL_MONARCHY';
+UPDATE Governments SET OtherGovernmentIntolerance = 0 WHERE  GovernmentType='GOVERNMENT_JFD_NOBLE_REPUBLIC';
