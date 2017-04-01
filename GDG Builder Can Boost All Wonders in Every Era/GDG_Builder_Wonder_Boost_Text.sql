24afc3d1235439bb089db9917058a567a63246b2
angelodgreat <carions46@gmail.com>
2017-04-02 07:14:05 +0800

    Implemented Text Information for GDG Builder WOnder Boost

---
+++ GDG Builder Can Boost All Wonders in Every Era/GDG_Builder_Wonder_Boost_Text.sql
@@ -0,0 +1,8 @@
+INSERT INTO LocalizedText (Tag, Language, Text)
+VALUES
+	('LOC_TRAIT_GDG_UNIVERSAL_WONDER_BOOST_NAME', 'en_US', 'GDG Builder Every Wonder Boost for Chosen Civ');
+
+	
+INSERT INTO LocalizedText (Tag, Language, Text)
+VALUES
+	('LOC_TRAIT_GDG_UNIVERSAL_WONDER_BOOST_DESCRIPTION', 'en_US', 'Allows your Chosen Civ to Wonder Boost every wonder in every Eras.');
