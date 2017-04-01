6bdf1634bc42d37083f5f90efe01db82311ae3ec
angelodgreat <carions46@gmail.com>
2017-04-01 13:36:23 +0800

    Removed All leads to rome and replace it to a new ability that only creates road instead using rome's ability

---
+++ GDG Roads to Capital/Text/GDG_RoadstoCapital_Text.sql
@@ -0,0 +1,8 @@
+INSERT INTO LocalizedText (Tag, Language, Text)
+VALUES
+	('LOC_TRAIT_GDG_CIVILIZATION_ROADS_TO_CAPITAL_NAME', 'en_US', 'GDG Road to Capital');
+
+	
+INSERT INTO LocalizedText (Tag, Language, Text)
+VALUES
+	('LOC_TRAIT_GDG_CIVILIZATION_ROADS_TO_CAPITAL_DESCRIPTION', 'en_US', 'Creates Road to your Capital City when you settle your Settler.');
