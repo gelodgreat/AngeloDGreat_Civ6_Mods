98e3c158aca6226ecd93f7068a08eadc8753c1d9
angelodgreat <carions46@gmail.com>
2017-04-03 09:44:45 +0800

    Fixed Text For GDG Chosen Unique, Added Released Version so it's easy to change.

---
+++ GDG [Personal] The Chosen Unique/GDG_TheChosenUniqueTraitsText.sql
@@ -0,0 +1,45 @@
+INSERT INTO LocalizedText (Tag, Language, Text)
+VALUES
+	('LOCTRAIT_GDG_CIVILIZATION_PRODUCTION_NAME', 'en_US', 'GDG City Production Boost'),
+	('LOC_TRAIT_CIVILIZATION_EXTRA_UNITS_NAME', 'en_US',  'GDG Extra Units'),
+	('LOC_GDG_CIVILIZATION_MO_IP_RSD_RSS_NAME', 'en_US', 'GDG City State Influence Points, Religious Spread Boost'),
+	('LOC_GDG_CIVILIZATION_ONEFORALL_MILITARY_UNITS_PROD_NAME', 'en_US', 'GDG Military Units Production Boost '),
+	('LOC_TRAIT_GDG_CIVILIZATION_BUILDING_PRODUCTION_NAME', 'en_US', 'GDG Building Production'),
+	('LOC_TRAIT_GDG_CIVILIZATION_FASTER_BORDER_GROWTH_NAME', 'en_US', 'GDG Faster Border Growth'),
+	('LOC_TRAIT_GDG_CIVILIZATION_MANY_BUILDER_CHARGE_NAME', 'en_US', 'GDG Builder Extra Charge'),
+	('LOC_TRAIT_GDG_CIVILIZATION_FASTEST_SETTLER_PROD_NAME', 'en_US', 'GDG Settler Production Boost'),
+	('LOC_TRAIT_GDG_CIVILIZATION_EXTRA_WILD_CARD_NAME', 'en_US', 'GDG Extra Wild Card'),
+	('LOC_TRAIT_GDG_CIVILIZATION_WONDER_PRODUCTION_NAME', 'en_US', 'GDG Wonder Production Boost'),
+	('LOC_TRAIT_GDG_CIVILIZATION_DISTRICT_PRODUCTION_NAME', 'en_US', 'GDG District Production Boost'),
+	('LOC_TRAIT_GDG_CIVILIZATION_UNIT_UPGRADE_DISCOUNT_NAME', 'en_US', 'GDG Unit Upgrade Discount'),
+	('LOC_TRAIT_GDG_CIVILIZATION_CITY_GROWTH_NAME', 'en_US', 'GDG City Growth Boost'),
+	('LOC_TRAIT_GDG_CIVILIZATION_CITY_TILES_NAME', 'en_US', 'GDG Extra City Tiles'),
+	('LOC_TRAIT_ALL_SUZERAIN_BONUSES_NAME', 'en_US', 'GDG Suzerain Bonuses'),
+	('LOC_TRAIT_GDG_CULTURE_BOMB_IMPROVEMENTS_NAME', 'en_US', 'GDG Culture Boooooomb'),
+	('LOC_TRAIT_GDG_CIVILIZATION_TRADE_YIELDS_NAME', 'en_US', 'GDG Trade Yields'),
+	('LOC_TRAIT_GDG_CIVILIZATION_PURCHASE_UNITS_FAITH_NAME', 'en_US', 'GDG Faith Currency');
+	
+
+	
+INSERT INTO LocalizedText (Tag, Language, Text)
+VALUES
+	('LOCTRAIT_GDG_CIVILIZATION_PRODUCTION_DESCRIPTION', 'en_US', 'Boost City Production.'),
+	('LOC_TRAIT_CIVILIZATION_EXTRA_UNITS_DESCRIPTION', 'en_US', 'Provides extra units after production.'),
+	('LOC_GDG_CIVILIZATION_MO_IP_RSD_RSS_DESCRIPTION', 'en_US', 'Provides Boost for Influence points and Religous spread.'),
+	('LOC_GDG_CIVILIZATION_ONEFORALL_MILITARY_UNITS_PROD_DESCRIPTION', 'en_US', 'Provides Military Unit Production.'),
+	('LOC_TRAIT_GDG_CIVILIZATION_BUILDING_PRODUCTION_DESCRIPTION', 'en_US', 'Provides Production Boost when constructing buildings.'),
+	('LOC_TRAIT_GDG_CIVILIZATION_FASTER_BORDER_GROWTH_DESCRIPTION', 'en_US', 'Increase Percentage of Border Growth.'),
+	('LOC_TRAIT_GDG_CIVILIZATION_MANY_BUILDER_CHARGE_DESCRIPTION', 'en_US', 'Provides extra charge for builders.'),
+	('LOC_TRAIT_GDG_CIVILIZATION_FASTEST_SETTLER_PROD_DESCRIPTION', 'en_US', 'Provides extra production when training a settler.'),
+	('LOC_TRAIT_GDG_CIVILIZATION_EXTRA_WILD_CARD_DESCRIPTION', 'en_US', 'Provides Extra Wild Card in your Civilization'),
+	('LOC_TRAIT_GDG_CIVILIZATION_WONDER_PRODUCTION_DESCRIPTION', 'en_US', 'Provides boost when constructing a Wonder.'),
+	('LOC_TRAIT_GDG_CIVILIZATION_DISTRICT_PRODUCTION_DESCRIPTION', 'en_US', 'Provides boos when constructing a District.'),
+	('LOC_GDG_CIVILIZATION_UNIT_UPGRADE_DISCOUNT_DESCRIPTION', 'en_US', 'Provides discount when upgrading units.'),
+	('LOC_TRAIT_GDG_CIVILIZATION_CITY_GROWTH_DESCRIPTION', 'en_US', 'Provides city growth boost.'),
+	('LOC_GDG_CIVILIZATION_CITY_TILES_DESCRIPTION', 'en_US', 'Provides extra tiles after settling a city.'),
+	('LOC_TRAIT_ALL_SUZERAIN_BONUSES_DESCRIPTION', 'en_US', 'Gives Suzerain Bonus of each City-State.'),
+	('LOC_TRAIT_GDG_CULTURE_BOMB_IMPROVEMENTS_DESCRIPTION', 'en_US', 'Culture Bomb when building improvements.'),
+	('LOC_TRAIT_GDG_CIVILIZATION_TRADE_YIELDS_DESCRIPTION', 'en_US', 'Provides Extra Bonus in trading.'),
+	('LOC_TRAIT_GDG_CIVILIZATION_PURCHASE_UNITS_FAITH_DESCRIPTION', 'en_US', 'Gives the ability to purchase units using faith.');
+	
+	
